1700e0579c50bbbfe90c72dc97022831e9d024ce
Sagar Bhattacharya <Sagar Bhattacharya>
2016-09-30 17:32:32 +0530

    added basic settigns

---
+++ www/bower_components/bower-framework7-angularjs/framework7.angular.hook.js
@@ -0,0 +1,40 @@
+Framework7.prototype.plugins.angular = function(app, params) {
+  function compile(newPage) {
+    try {
+      var $page = $(newPage);
+      var injector = angular.element("[ng-app]").injector();
+      var $compile = injector.get("$compile");
+      var $timeout = injector.get("$timeout");
+      var $scope = injector.get("$rootScope");
+      $scope = $scope.$$childHead;
+      $timeout(function() {
+        $compile($page)($scope);
+      })
+    } catch (e) {
+      //console.error("Some Error Occured While Compiling The Template", e);
+    }
+  }
+
+  function removeOldPage(pageData){
+    var $oldPage =  $(".views .view .pages .page").not( $(pageData.container));
+    if( $oldPage.length > 0){
+      var controllerName = $oldPage.attr("ng-controller");
+      var $scope = angular.element('[ng-controller='+controllerName+']').scope();
+      if($scope){
+        $scope.$destroy();
+        $oldPage.remove();
+      }
+    }
+  }
+
+  return {
+    hooks: {
+      pageInit: function(pageData) {
+        compile(pageData.container);
+      },
+      pageAfterAnimation  : function(pageData){
+        removeOldPage(pageData);
+      }
+    }
+  }
+};
